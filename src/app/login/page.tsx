4d1c348632a43e0c743fb01e4d350d855e82b869
vilarj <lamaravillasan@outlook.com>
2024-12-18 15:51:39 -0400

    Cleaning up

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,59 +1,7 @@
-"use client";
-<<<<<<< HEAD
-import Image from "next/image";
-import { useState } from "react";
-import image from "../../../public/KeepTrack.png";
-=======
-import { useState } from "react";
->>>>>>> d576516 (All routes work properly | Placeholders in place | TODO: Need to work on the login logic and the styling of all new placeholders)
 import styles from "../styles/login.module.css";
 
-function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    console.log("Logging in with:", { email, password });
-  };
-
-  return (
-    <section className={styles.login}>
-<<<<<<< HEAD
-      <Image
-        priority
-        src={image}
-        height={150}
-        width={350}
-        alt="KeepTrack Logo"
-      />
-
-=======
-      <h2>Login</h2>
->>>>>>> d576516 (All routes work properly | Placeholders in place | TODO: Need to work on the login logic and the styling of all new placeholders)
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Login</button>
-      </form>
-    </section>
-  );
+function login() {
+  return <div className={styles.login}>login</div>;
 }
 
-export default Login;
+export default login;
